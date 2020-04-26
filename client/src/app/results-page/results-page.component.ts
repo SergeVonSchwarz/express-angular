bce5f52bc18017f8a14a4b9a4a6b374bb4481857
sergio <sergio.glock@gmail.com>
2020-04-26 11:23:32 +0300

    results table

--- client/src/app/results-page/results-page.component.ts
+++ client/src/app/results-page/results-page.component.ts
@@ -11,10 +11,15 @@ import {Observable} from 'rxjs/index';
 export class ResultsPageComponent implements OnInit {
 
   answers$: Observable<Answer[]>;
+  arrForTemplate = [0,1,2,3,4,5,6,7,8,9];
 
   constructor(private quizService: QuizService) { }
 
   ngOnInit() {
     this.answers$ = this.quizService.fetch();
+  }
+
+  sum(arr) {
+    return arr.reduce((sum, val) => +sum + +val);
   }
 }
